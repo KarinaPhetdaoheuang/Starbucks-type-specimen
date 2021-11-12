15789fbd8e655c01ce291f8e819d2cf26e06290c
KarinaPhetdaoheuang <89856849+KarinaPhetdaoheuang@users.noreply.github.com>
2021-11-11 20:37:46 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 (function () {
   "use strict";
 
-  // Add Code Below Here
+  document.querySelector(".box.box1.").addEventListener("mouseover", (e) => {
+    e.target.setAttribute("title", document.querySelector("div.innerText p").innerText);
+});
+  
+  document.querySelector(".box.box2.").addEventListener("mouseover", (e) => {
+    e.target.setAttribute("title", document.querySelector("div.innerText p").innerText);
+});
 
+  document.querySelector(".box.box3.").addEventListener("mouseover", (e) => {
+    e.target.setAttribute("title", document.querySelector("div.innerText p").innerText);
+});
+  
+  document.querySelector(".box.box4.").addEventListener("mouseover", (e) => {
+    e.target.setAttribute("title", document.querySelector("div.innerText p").innerText);
+});
+  
+  document.querySelector(".box.box5.").addEventListener("mouseover", (e) => {
+    e.target.setAttribute("title", document.querySelector("div.innerText p").innerText);
+});
+  
+  document.querySelector(".box.box6.").addEventListener("mouseover", (e) => {
+    e.target.setAttribute("title", document.querySelector("div.innerText p").innerText);
+});
 })();
+
+
